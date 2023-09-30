bc39a0048b2dba69d7e02120ac11f9c47d1d9dea
camilaalvarado <mariacamilaalvarado980@gmail.com>
2023-09-30 15:26:33 -0400

    camila alvarado

---
+++ tarea1.py
@@ -0,0 +1,10 @@
+#Define una variable de cada tipo de primitivo
+numero = 5
+flotante = 4.5
+maestro = True
+cadena = "str"
+#Concatena a la cadena las otras variables aplicando la conversión correcta para funcionar, guarda el resultado en una variable
+concatena = cadena + str(numero) + str(flotante) + str(maestro)
+print (concatena)
+
+
