3a58181b45906c65b3721fad6c7cbbb8df918374
camilaalvarado <mariacamilaalvarado980>
2023-10-02 00:36:00 -0400

    updated

--- adaschool.py
+++ adaschool.py
@@ -6,7 +6,9 @@ cadena = "str"
 #Concatena a la cadena las otras variables aplicando la conversión correcta para funcionar, guarda el resultado en una variable
 concatena = cadena + " "+ str(numero) + " "+  str(flotante) +" "+ str(maestro)
 print (concatena)
-#el limite de un entero es:  ,
+#Investigación
+#Los enteros en Python no tienen un límite fijo. El límite es la memoria disponible, Para conocer el límite en tu sistema, puedes utilizar sys.maxsize o sys.maxint.
+#Los flotantes en Python siguen el estándar IEEE 754 y tienen límites definidos. El límite superior es aproximadamente 1.8 x 10^308, y el límite inferior es aproximadamente -1.8 x 10^308
 #Aplica la fórmula de la suma de los primeros n números pares (investigar), tomando como n la variable de tipo entero y almacenar el resultado en una variable
 n = 20
 resultado = 0
