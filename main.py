fb0367f2fcf9c5c1f8b188514a6221743e13638a
camilaalvarado <mariacamilaalvarado980@gmail.com>
2023-11-03 01:04:34 -0400

    proyecto integrador

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Proyecto integrador parte 1
 
 #vamos a pedir al jugador su nombre de personaje y vamos a imprimir un mensaje de bienvenida 
-#con el nombre qué se ingresó.
+#con el nombre qué se ingresó
 
 
 nombre = input ("Introduce tu nombre: ")
