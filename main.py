6b78297f5af956d76b71e0778aa070205e36a634
camilaalvarado <mariacamilaalvarado980@gmail.com>
2023-11-02 21:07:21 -0400

    update proyecto integrador

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-#Proyecto integrador parte 1
+#Proyecto integrador parte 1.
 
 #vamos a pedir al jugador su nombre de personaje y vamos a imprimir un mensaje de bienvenida 
 #con el nombre qué se ingresó.
